89ae09b4d771e74e43701d00cf4896ad57a16bef
gayratovdev <gayratovdev@gmail.com>
2023-02-02 11:39:17 +0500

    edit-two

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom/client';
 
 const text = ReactDOM.createRoot(document.querySelector('.wrapper'))
 
-text.render(<h1>Hello Algorithm! <br/> Welcome to ZAFARBEK Project!</h1>)
+text.render(<h1>Hello Algorithm Team! <br/> Welcome to ZAFARBEK Project!</h1>)
